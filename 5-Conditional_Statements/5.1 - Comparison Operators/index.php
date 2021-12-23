f217a5e5c4ac8f83b98bb89e9560679e9d58b000
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2021-12-23 12:02:57 +0100

    php 5

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,21 @@
+<?php
+$test1 = 10;
+$test2 = "gewoon tekst";
+
+var_dump($test1==$test2);
+echo "<br>";
+var_dump($test1 === $test2);
+echo "<br>";
+var_dump($test1 != $test2);
+echo "<br>";
+var_dump($test1 > $test2);
+echo "<br>";
+var_dump($test1 < $test2);
+echo "<br>";
+var_dump($test1 <> $test2);
+echo "<br>";
+var_dump($test1 >= $test2);
+echo "<br>";
+var_dump($test1 <= $test2);
+
+?>
