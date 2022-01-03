d830f211e71e5b55b9475fb200d98263f948be5c
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-01-03 18:36:18 +0100

    php 5.3

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,51 @@
+<?php
+$test1 = 3;
+$test2 = 6;
+
+ if ($test1 == $test2) {
+     echo "True";
+ } else {
+     echo "false";
+ }
+
+ echo '<br>';
+ 
+ if ($test1 !== $test2) {
+    echo "True";
+} else {
+    echo "false";
+}
+
+echo '<br>';
+
+if ($test1 > $test2) {
+    echo "True";
+} else {
+    echo "false";
+}
+
+echo '<br>';
+
+if ($test1 < $test2) {
+    echo "True";
+} else {
+    echo "false";
+}
+
+echo '<br>';
+
+if ($test1 >= $test2) {
+    echo "True";
+} else {
+    echo "false";
+}
+
+echo '<br>';
+
+if ($test1 <= $test2) {
+    echo "True";
+} else {
+    echo "false";
+}
+
+?>
