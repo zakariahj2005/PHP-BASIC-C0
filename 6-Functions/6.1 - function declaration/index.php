05693298ec71ccd12767ed6d5d7c124db891b276
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-01-10 10:35:04 +0100

    php 6.1

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,11 @@
+<?php
+
+function zetTweeWoordenAanElkaar(){
+$woordje = 'hallo ';
+$zinnetje = "mooie blauwe planeet!";
+$helezin = $woordje . $zinnetje;
+echo $helezin;
+}
+
+zetTweeWoordenAanElkaar();
+?>         
